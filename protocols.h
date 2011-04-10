68cf51dea2c84716cb0016e6462aa3806f3fd719
'Nicholas Capo <nicholas.capo@gmail.com>
2011-04-10 00:57:35 -0500

    Added header blocks and empty main functions to source code, confirmend that make works

---
+++ protocols.h
@@ -0,0 +1,15 @@
+/**
+ * Name: Andy Aloia and Nicholas Capo
+ * Course: COSC 4653 - Advanced Networks
+ * Assignment: PROJECT - Multiserver Chat Rooms in TCP and UDP
+ * File name: protocols.h
+ * Purpose: 
+ * Usage: 
+ *
+ * Limitations: 
+ * Development Computer: x86
+ * Operating System: Ubuntu Linux 10.04
+ * Integrated Development Environment (IDE): Netbeans 6.9.1
+ * Compiler: gcc (Ubuntu 4.4.3-4ubuntu5) 4.4.3
+ * Operational Status: 
+ **/
---
+++ registrationServer.c
@@ -0,0 +1,33 @@
+/**
+ * Name: Andy Aloia and Nicholas Capo
+ * Course: COSC 4653 - Advanced Networks
+ * Assignment: PROJECT - Multiserver Chat Rooms in TCP and UDP
+ * File name: registrationServer.c
+ * Purpose: 
+ * Usage: 
+ *
+ * Limitations: 
+ * Development Computer: x86
+ * Operating System: Ubuntu Linux 10.04
+ * Integrated Development Environment (IDE): Netbeans 6.9.1
+ * Compiler: gcc (Ubuntu 4.4.3-4ubuntu5) 4.4.3
+ * Operational Status: 
+ **/
+ 
+//INCLUDES ##########################################################
+#include "protocols.h"
+#include "wrapperFunctions.c"
+
+// CONSTANTS ########################################################
+
+// GLOBALS ##########################################################
+
+// PROTOTYPES #######################################################
+
+// MAIN #######################################################
+
+int main(int argc, char* argv[]){
+return 0;
+}
+
+//  #######################################################
