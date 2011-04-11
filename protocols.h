0bec642ad64aa368ec524abb9102ef90070c5dd5
Nicholas Capo <Nicholas Capo'@localhost>
2011-04-11 12:59:10 -0500

    Added Room and ChatMessage structs to protocols.h

--- protocols.h
+++ protocols.h
@@ -3,8 +3,8 @@
  * Course: COSC 4653 - Advanced Networks
  * Assignment: PROJECT - Multiserver Chat Rooms in TCP and UDP
  * File name: protocols.h
- * Purpose: 
- * Usage: 
+ * Purpose: Provides structs for communication types
+ * Usage: None
  *
  * Limitations: 
  * Development Computer: x86
@@ -13,3 +13,37 @@
  * Compiler: gcc (Ubuntu 4.4.3-4ubuntu5) 4.4.3
  * Operational Status: 
  **/
+ 
+#ifndef PROTOCOLS
+#define PROTOCOLS
+
+#define MAX_USER_ID_LENGTH 30
+#define MAX_MESSAGE_TEXT 160
+#define MAX_IPADDR_STRING_LENGTH 15
+
+#define STATUS_JOIN 0
+#define STATUS_ONLINE 1
+#define STATUS_LEAVE 2
+#define REGISTER_NONE 3
+#define REGISTER_REQUEST 4
+#define REGISTER_CONFIRM 5
+#define REGISTER_LEAVE 6
+
+ // Used for Registration of Room Server
+ // and room lists sent to client by Registration Server
+ typedef struct {
+ 	char name[MAX_USER_ID_LENGTH];
+  	char address[MAX_IPADDR_STRING_LENGTH];
+ 	int request;
+ }Room;
+ 
+ // Normal Chat Messages
+ typedef struct{
+ 	char user[MAX_USER_ID_LENGTH];
+ 	int status;
+ 	char text[MAX_MESSAGE_TEXT];
+ }ChatMessage;
+
+
+#endif 
+
