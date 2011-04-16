e716b1472dfe0d2702abf3a7e3c5753b9124b478
spiffydudex <spiffydudex@localhost>
2011-04-15 19:05:23 -0500

    Added the Purpose statement to some files. Changed the Operational Status to "Compiles"
    Submitted increment 1
    ref 6 closed

--- client.c
+++ client.c
@@ -3,7 +3,8 @@
  * Course: COSC 4653 - Advanced Networks
  * Assignment: PROJECT - Multiserver Chat Rooms in TCP and UDP
  * File name: client.c
- * Purpose: 
+ * Purpose: Creates a TCP connection to the registration server to receive chat 
+ * rooms. Connects to a chat room via a TCP or UDP connection.
  * Usage: client <Server IP address> <Server Port> <nickname>
  *
  * Limitations: 
@@ -11,7 +12,7 @@
  * Operating System: Ubuntu Linux 10.04
  * Integrated Development Environment (IDE): Netbeans 6.9.1
  * Compiler: gcc (Ubuntu 4.4.3-4ubuntu5) 4.4.3
- * Operational Status: 
+ * Operational Status: Compiles, not complete.
  **/
 
 //INCLUDES ##########################################################
--- wrapperFunctions.c
+++ wrapperFunctions.c
@@ -11,7 +11,7 @@
  * Operating System: Ubuntu Linux 10.04
  * Integrated Development Environment (IDE): Netbeans 6.9.1
  * Compiler: gcc (Ubuntu 4.4.3-4ubuntu5) 4.4.3
- * Operational Status: 
+ * Operational Status: Compiles
  **/
 
 //##############################################################
