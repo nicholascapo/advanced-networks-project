55bdafe8822912b1f12274da65b98d22e1742c5e
'Nicholas Capo' <'Nicholas Capo'@localhost>
2011-04-26 21:08:07 -0500

    Added Copyright notice to sources and Copy of GNU GPL 3

--- client.c
+++ client.c
@@ -5,6 +5,21 @@
  * File name: client.c
  * Purpose: Creates a TCP connection to the registration server to receive chat 
  * messages. Allows the user to send messages to the room
+ * 
+ * Copyright (C) 2011 Andy Aloia and Nicholas Capo
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  **/
 
 //INCLUDES ##########################################################
